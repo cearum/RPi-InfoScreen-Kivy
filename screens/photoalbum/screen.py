8dbc4a61c8087825e12e32c3fe7bc1e944dec12a
cearum <33786368+cearum@users.noreply.github.com>
2020-01-13 21:51:07 -0500

    Add Gestures to PhotoAlbum to go Forward and Backward

--- screens/photoalbum/screen.py
+++ screens/photoalbum/screen.py
@@ -6,14 +6,31 @@ from kivy.properties import (ObjectProperty,
                              StringProperty,
                              BoundedNumericProperty)
 from kivy.uix.label import Label
-from kivy.uix.screenmanager import Screen
+from kivy.uix.screenmanager import Screen, SlideTransition, FadeTransition
+from kivy.gesture import GestureDatabase
+from kivy.gesture import Gesture
+from kivy.uix.floatlayout import FloatLayout
+
+from functools import partial
 from random import shuffle
 from imghdr import what
+
+from core.my_gestures import left_to_right_line_str, right_to_left_line_str
+gesture_strings = {
+    'left_to_right_line': left_to_right_line_str,
+    'right_to_left_line': right_to_left_line_str,
+}
+gestures = GestureDatabase()
+for name, gesture_string in gesture_strings.items():
+    gesture = gestures.str_to_gesture(gesture_string)
+    gesture.name = name
+    gestures.add_gesture(gesture)
 
 
 class BlackHole(object):
     def __init__(self, **kw):
         super(BlackHole, self).__init__()
+
 
 class Photo(Screen):
     """Screen object to display a photo."""
@@ -29,6 +46,60 @@ class PhotoLoading(Screen):
        photos.
     """
     pass
+
+
+class GestureBox(FloatLayout):
+
+    alert_dialog = ObjectProperty()
+
+    def __init__(self, **kwargs):
+        for gesture_name in gesture_strings:
+            self.register_event_type('on_{}'.format(gesture_name))
+        super(GestureBox, self).__init__(**kwargs)
+
+        self.interval = 0
+        self.alert_dismissal = None
+
+    def on_left_to_right_line(self):
+        pass
+
+    def on_right_to_left_line(self):
+        pass
+
+    def on_top_to_bottom_line(self):
+        pass
+
+    def on_bottom_to_top_line(self):
+        pass
+
+    # To recognize a gesture, you’ll need to start recording each individual event in the
+    # touch_down handler, add the data points for each call to touch_move , and then do the
+    # gesture calculations when all data points have been received in the touch_up handler.
+
+    def on_touch_down(self, touch):
+        # create an user defined variable and add the touch coordinates
+        touch.ud['gesture_path'] = [(touch.x, touch.y)]
+        super(GestureBox, self).on_touch_down(touch)
+
+    def on_touch_move(self, touch):
+        touch.ud['gesture_path'].append((touch.x, touch.y))
+        super(GestureBox, self).on_touch_move(touch)
+
+    def on_touch_up(self, touch):
+        if 'gesture_path' in touch.ud:
+            # create a gesture object
+            this_gesture = Gesture()
+            # add the movement coordinates
+            this_gesture.add_stroke(touch.ud['gesture_path'])
+            # normalize so thwu will tolerate size variations
+
+            this_gesture.normalize()
+            # minscore to be attained for a match to be true
+            match = gestures.find(this_gesture, minscore=0.3)
+            if match:
+                #print("{} happened".format(match[1].name))
+                self.dispatch('on_{}'.format(match[1].name))
+        super(GestureBox, self).on_touch_up(touch)
 
 
 class PhotoAlbumScreen(Screen, BlackHole):
@@ -68,11 +139,11 @@ class PhotoAlbumScreen(Screen, BlackHole):
 
             # Retrieve photos
             Clock.schedule_once(self.getPhotos, 0.5)
+            Clock.schedule_once(partial(self.showPhoto, 'forward'), self.photoduration)
 
         else:
             # We've been here before so just show the photos
-            self.timer = Clock.schedule_interval(self.showPhoto,
-                                                 self.photoduration)
+            self.timer = Clock.schedule_interval(partial(self.showPhoto, 'forward'), self.photoduration)
 
     def on_leave(self):
 
@@ -114,17 +185,41 @@ class PhotoAlbumScreen(Screen, BlackHole):
         self.running = True
 
         # and start the timer
-        self.timer = Clock.schedule_interval(self.showPhoto,
-                                             self.photoduration)
+        self.timer = Clock.schedule_interval(partial(self.showPhoto, 'forward'), self.photoduration)
 
         # Show the first photo
-        self.showPhoto()
+        self.showPhoto(direction="forward")
 
-    def showPhoto(self, *args):
+    def swipe_photo(self, direction="forward"):
+        # unschedule current clock
+        Clock.unschedule(self.timer)
+        self.photoscreen.transition = SlideTransition()
+        if direction == 'forward':
+            self.photoscreen.transition.direction = 'left'
+        elif direction == 'back':
+            self.photoscreen.transition.direction = 'right'
+
+        # Reschedule Clock
+        self.timer = Clock.schedule_interval(partial(self.showPhoto, 'forward'), self.photoduration)
+
+        # show the photo
+        self.showPhoto(direction)
+
+        self.photoscreen.transition = FadeTransition()
+
+    def showPhoto(self, direction="forward", *args):
         """Method to update the currently displayed photo."""
+        try:
+            print(direction, args)
+        except ValueError:
+            print(direction)
 
         # Get the current photo
-        photo = self.photos[self.photoindex]
+        if direction == "forward":
+            photo = self.photos[self.photoindex]
+        elif direction == "back":
+            self.photoindex = (self.photoindex - 2) % len(self.photos)
+            photo = self.photos[self.photoindex]
 
         # Create a screen object to show that photo
         scr = Photo(name=photo)
